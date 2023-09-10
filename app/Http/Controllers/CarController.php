2ce29e3539a339310c9b2dfa38b2e26d94594909
daniilkoversun <koversundanil@gmail.com>
2023-09-10 18:19:49 +0300

    Changes

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -7,9 +7,11 @@ use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
+    protected int $limit = 6;
+
     public function cars()
     {
-        $cars = DokStoreFacade::getCars();
-
+        $cars = array_slice(DokStoreFacade::getCars(), 0, $this->limit);
+        return view('welcome', compact('cars'));
     }
 }
